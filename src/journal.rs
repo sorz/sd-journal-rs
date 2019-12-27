582e47cdb56dc76a452b8480b014c39b37b6c3b0
sorz <me@sorz.org>
2019-12-27 15:11:07 +0800

    Add get entry field

--- src/journal.rs
+++ src/journal.rs
@@ -15,12 +15,16 @@ use crate::{
         sd_journal_previous,
         sd_journal_next_skip,
         sd_journal_previous_skip,
+        sd_journal_get_data,
     },
 };
-use std::marker::PhantomData;
+use std::{
+    fmt,
+    ffi::{c_void, CString},
+};
 
 #[derive(Debug, Clone)]
-enum Seek {
+pub enum Seek {
     Head,
     Tail,
     Monotonic {
@@ -33,7 +37,7 @@ enum Seek {
 }
 
 #[derive(Debug)]
-struct Journal {
+pub struct Journal {
     ret: *mut SdJournal,
 }
 
@@ -43,6 +47,16 @@ impl Drop for Journal {
             sd_journal_close(self.ret);
         }
     }
+}
+
+pub struct Entry<'j> {
+    journal: &'j Journal,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Field<'e> {
+    pub name: &'e str,
+    pub data: &'e [u8],
 }
 
 impl Journal {
@@ -82,13 +96,12 @@ impl Journal {
         Ok(())
     }
 
-
     pub fn next(&mut self) -> SdResult<bool> {
         match unsafe { sd_journal_next(self.ret) } {
             0 => Ok(false),
             1 => Ok(true),
             e => Err(e),
-        }        
+        }
     }
 
     pub fn previous(&mut self) -> SdResult<bool> {
@@ -96,7 +109,7 @@ impl Journal {
             0 => Ok(false),
             1 => Ok(true),
             e => Err(e),
-        }        
+        }
     }
 
     pub fn skip(&mut self, n: i64) -> SdResult<u64> {
@@ -111,16 +124,58 @@ impl Journal {
             Ok(ret as u64)
         }
     }
+
+    pub fn entry(&mut self) -> Entry {
+        Entry { journal: self }
+    }
 }
 
+impl Entry<'_> {
+    pub fn field<S: AsRef<str>>(&self, name: S) -> SdResult<Field> {
+        let c_name = CString::new(name.as_ref()).unwrap();
+        let mut buf = 0 as *const u8;
+        let mut size = 0usize;
+
+        checked_unsafe_call! {
+            sd_journal_get_data(
+                self.journal.ret,
+                c_name.as_ptr(),
+                &mut buf as *mut _ as *mut *const c_void,
+                &mut size,
+            )
+        };
+        let buf = unsafe { std::slice::from_raw_parts(buf, size) };
+        let field = Field::from_raw(buf);
+        assert_eq!(name.as_ref(), field.name);
+        Ok(field)
+    }
+}
+
+impl<'a> Field<'a> {
+    fn from_raw(data: &'a [u8]) -> Self {
+        let mut name_data = data.splitn(2, |c| *c == b'=');
+        let name = name_data.next().unwrap();
+        let data = name_data.next().expect("missing field name");
+        let name = std::str::from_utf8(name).expect("invalid utf-8 field name");
+        Field { name, data }
+    }
+}
+
+impl fmt::Display for Field<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}={}", self.name, String::from_utf8_lossy(self.data))
+    }
+}
 
 #[test]
 fn test_open() {
-    let mut journal = Journal::open(OpenFlags::CURRENT_USER).unwrap();
+    let mut journal = Journal::open(OpenFlags::empty()).unwrap();
     println!("usage {}", journal.usage().unwrap());
     journal.seek(Seek::Head).unwrap();
     println!("seek head done");
-    journal.next().unwrap();
+    assert!(journal.next().unwrap());
     println!("next head done");
+    let entry = journal.entry();
+    println!("field {}", entry.field("MESSAGE_ID").unwrap());
 
 }
